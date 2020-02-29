52d14014bd0f2e029c27b95dcd10c6600b325703
mkrasienko <mkrasienko@gamil.com>
2020-02-29 13:30:53 +0100

    Initial commit

---
+++ geo_app.py
@@ -0,0 +1,8 @@
+import json
+import urllib
+another_address = 'Y'
+
+while another_address =='Y':
+    address = input(print('Please enter an address: '))
+    another_address = input(print('Would you like to enter another address? '))
+
